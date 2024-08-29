8a811fcd191a9830f85e871738ce2102a55d7eed
typeling1578 <pub@typeling1578.dev>
2024-08-29 13:31:08 +0900

    Remove unused weights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
 });
 
