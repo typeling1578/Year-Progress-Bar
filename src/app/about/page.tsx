7214541b4751faeb9aa21554d7f13cbe7efa4b60
typeling1578 <pub@typeling1578.dev>
2024-09-26 08:40:07 +0900

    adjust font-size

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function About() {
   return (
     <RootElement>
       <div className="w-full h-full flex flex-col justify-center items-center gap-6 text-center">
-        <h1 className="text-[32px] font-extrabold">About</h1>
+        <h1 className="text-[34px] font-extrabold">About</h1>
         <div className="font-semibold">
           This site displays the progress of the year.
           <br />
